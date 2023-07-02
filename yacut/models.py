a28257c34b0593fafeb41b42d95fcc030a5ba55b
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-02 20:09:37 +0400

    add methods - to_dict, from_dict

--- yacut/models.py
+++ yacut/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from flask import url_for
+
 from . import db
 
 
@@ -9,3 +11,31 @@ class URLMap(db.Model):
     original = db.Column(db.String, nullable=False)
     short = db.Column(db.String(16), nullable=False, unique=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # метод для преобразования сложных данных(экземпляра модели)
+    # в словарь
+    # по сути этот метод выполняет сериализацию данных
+    def to_dict(self):
+        """Метод-сериализатор."""
+        return dict(
+            url=self.original,
+            # функция url_for - принимает на вход функцию и возвращает URLдля нее
+            # т.о осуществляется переход на нужную страницу
+            # параметр external=True говорит о том, что фласку нужно сформировать абсолютную,
+            # а не относительную ссылку. К этой абсолютной ссылке добавляем короткий идентификатор
+            custom_id=url_for(
+                'main_view', _external=True) + self.short
+        )
+
+    # добавим в модель новый метод-десериализатор
+    # на вход этот метод принимает словарь data, полученный
+    # из JSON в запросе
+    # его задача - добавить в пустой объект класса URLMap
+    # значения полей, которые будут переданы в POST-запросе
+    def from_dict(self, data):
+        """Метод-десериализатор."""
+        for field_key, field_item in {'original': 'url', 'short': 'custom_id'}.items():
+            # для каждой пары ключ:значение выполняем проверку
+            # есть ли значение в data
+            if field_item in data:
+                setattr(self, field_key, data[field_item])
