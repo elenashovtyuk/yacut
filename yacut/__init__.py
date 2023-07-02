4f6b312955f5f97edeaee918ab29e9979beb12ee
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-07-03 02:48:00 +0400

    delete my comments and add constants in any file

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,27 +1,14 @@
-# этот файл __init__.py объединяет все файлы, все модули в пакет
-# и этот пакет и будет представлять наше приложение
-
-# что должно быть в этом файле:
-# 1. подключение настроек
-# 2. создание экземпляра класса Flask - экземпляра приложения
-# 3. создание экземпляра БД
-
 from flask import Flask
-
 from flask_migrate import Migrate
-
 from flask_sqlalchemy import SQLAlchemy
 
 from settings import Config
 
-
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
-from . import api_views, error_handlers, views, forms, models
+from . import api_views, error_handlers, forms, models, views
--- yacut/models.py
+++ yacut/models.py
@@ -4,6 +4,8 @@ from flask import url_for
 
 from . import db
 
+FIELDS = {'original': 'url', 'short': 'custom_id'}
+
 
 class URLMap(db.Model):
 
@@ -12,30 +14,16 @@ class URLMap(db.Model):
     short = db.Column(db.String(16), nullable=False, unique=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # метод для преобразования сложных данных(экземпляра модели)
-    # в словарь
-    # по сути этот метод выполняет сериализацию данных
     def to_dict(self):
         """Метод-сериализатор."""
         return dict(
             url=self.original,
-            # функция url_for - принимает на вход функцию и возвращает URLдля нее
-            # т.о осуществляется переход на нужную страницу
-            # параметр external=True говорит о том, что фласку нужно сформировать абсолютную,
-            # а не относительную ссылку. К этой абсолютной ссылке добавляем короткий идентификатор
             custom_id=url_for(
                 'main_view', _external=True) + self.short
         )
 
-    # добавим в модель новый метод-десериализатор
-    # на вход этот метод принимает словарь data, полученный
-    # из JSON в запросе
-    # его задача - добавить в пустой объект класса URLMap
-    # значения полей, которые будут переданы в POST-запросе
     def from_dict(self, data):
         """Метод-десериализатор."""
-        for field_key, field_item in {'original': 'url', 'short': 'custom_id'}.items():
-            # для каждой пары ключ:значение выполняем проверку
-            # есть ли значение в data
+        for field_key, field_item in FIELDS.items():
             if field_item in data:
                 setattr(self, field_key, data[field_item])
